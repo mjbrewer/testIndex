45db5f8362135c22dc870eca0997b5fbd4200014
mjbrewer <mjbrewer@us.ibm.com>
2016-01-19 16:58:45 -0600

    Update Project.swift

--- Project.swift
+++ Project.swift
@@ -1,1 +1,8 @@
-test
+import PackageDescription
+
+let package = Package(
+    name: "Hello",
+    testDependencies: [
+        .Package(url: "ssh://git@example.com/Tester.git", versions: Version(1,0,0)..<Version(2,0,0)),
+    ]
+)
