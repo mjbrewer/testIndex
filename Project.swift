daa32e36610011560add346e18e7c5e0e0f04f9f
mjbrewer <mjbrewer@us.ibm.com>
2016-01-19 18:06:47 -0600

    Update Project.swift

--- Project.swift
+++ Project.swift
@@ -6,3 +6,11 @@ let package = Package(
         .Package(url: "ssh://git@example.com/Tester.git", versions: Version(1,0,0)..<Version(2,0,0)),
     ]
 )
+import PackageDescription
+
+let package = Package(
+    name: "Dealer",
+    dependencies: [
+        .Package(url: "https://github.com/apple/example-package-deckofplayingcards.git", majorVersion: 1),
+    ]
+)
