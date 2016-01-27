eb01bf7752da07a51a462487d7f25fe2914d382f
mjbrewer <mjbrewer@us.ibm.com>
2016-01-27 15:08:20 -0600

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+import PackageDescription
+
+let package = Package(
+    name: "Hello",
+    testDependencies: [
+        .Package(url: "ssh://git@example.com/Tester.git", versions: Version(1,0,0)..<Version(2,0,0)),
+    ]
+)
+import PackageDescription
+
+let package = Package(
+    name: "Dealer",
+    dependencies: [
+        .Package(url: "https://github.com/apple/example-package-deckofplayingcards.git", majorVersion: 1),
+    ]
+)
